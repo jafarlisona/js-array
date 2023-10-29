330baa13f47cc944e46ae25d4e35d2adfc99325d
jafarlisona <147070818+jafarlisona@users.noreply.github.com>
2023-10-29 22:34:17 +0400

    new commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1.Istenilen sayda  gelen datalarin Ortalamasini veren Js function yazin.Call back istifade edeceksiz.
+// const data = [20, 12, 13, 7, 9, 3, 8, 11, 22, 14];
+// let average = 0;
+// let sum = 0;
+// data.forEach((value,index,array) =>{
+//     sum += value;
+//     average= sum / array.length
+//     return average;
+// });
+// console.log(average);
+// 2.Istenilen sayda gelen datalarin cut olanlarinin toplamini tapan Js function yazin.Call back istifade edeceksiz.
+// const data = [20, 12, 13, 7, 9, 3, 8, 11, 22, 14];
+// let sumCut = 0;
+// data.forEach((value) => (value % 2 === 0 ? (sumCut += value) : null));
+// console.log(sumCut);
+// 3.Istenilen sayda gelen datalarin tek olanlarinin toplamini tapan Js function yazin.Call back istifade edeceksiz.
+// const data = [20, 12, 13, 7, 9, 3, 8, 11, 22, 14];
+// let sumTek = 0;
+// data.forEach((value) => (value % 2 !== 0 ? (sumTek += value) : null));
+// console.log(sumTek);
+// 4.Istenilen sayda gelen datalarin cut olanlarin toplamini tek olanlarinin toplamina olan hasilini tapan Js function yazin.Call back istifade edeceksiz.
+// const data = [20, 12, 13, 7, 9, 3, 8, 11, 22, 14];
+// let cutSum = 0;
+// let tekSum = 0;
+// let product = 1;
+// data.forEach((value) =>
+//   value % 2 === 0 ? (cutSum += value) : (tekSum += value)
+// );
+// product = cutSum * tekSum;
+// console.log(product);
